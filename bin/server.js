d82b518a74e3b1a7829b94c1c4606fde40dc3edd
Alexander Marchenko <maappdev@gmail.com>
2014-06-17 12:14:22 +0300

    fix server

--- bin/server.js
+++ bin/server.js
@@ -1,19 +1,19 @@
 var server = require('http').Server(),
 	debug = require('debug')('fancyflirt'),
 	spawn = require('child_process').spawn,
-	web = spawn('php', ['-S','127.0.0.1:'+(process.env.WEB || 8081),'-t','www/']),
+	//web = spawn('php', ['-S','127.0.0.1:'+(process.env.WEB || 8081),'-t','www/']),
 	app = require('../app/app')(server);
 
-web.stdout.on('data', function (data) {
-	debug(data);
-});
+// web.stdout.on('data', function (data) {
+// 	debug(data);
+// });
 
-web.stderr.on('data', function (data) {
-	debug(data);
-});
+// web.stderr.on('data', function (data) {
+// 	debug(data);
+// });
 
-server.listen(process.env.PORT || 3000, function () {
-	debug("Server listening on port [%d]", process.env.PORT || 3000);
-});
+// server.listen(process.env.PORT || 3000, function () {
+// 	debug("Server listening on port [%d]", process.env.PORT || 3000);
+// });
 
 
